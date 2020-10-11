a664189212f386a45d6e13c026b91eb5deec15d5
sriyagopalan <66567240+sriyagopalan@users.noreply.github.com>
2020-10-11 09:48:30 -0700

    Add files via upload

---
+++ block.js
@@ -0,0 +1,30 @@
+class Block{
+  constructor(x, y, width, height,) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.5,
+          'density':1.0,
+         
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibility = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      if (this.body.speed<3){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink")
+      rect(pos.x, pos.y, this.width, this.height);
+      }
+      else{
+        this.visibility = this.visibility-5;
+        push();
+        World.remove(world,this.body);
+        tint(255, this.visibility);
+        pop();
+      }
+    }
+}
